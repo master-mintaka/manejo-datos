575a0ed0934c54c684cf8da841eb67ea38ec1478
John W Granados A - Master Mintaka <jwga76@gmail.com>
2023-08-31 22:44:56 -0500

    Validacion password

--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -8,5 +8,26 @@ class ValidateTest extends TestCase
     {
         $email = Validate::email('jwga76@gmail.com');
         $this->assertTrue($email);
+        
+        $email = Validate::email('jwga76@@gmail.com');
+        $this->assertFalse($email);
+    }
+
+    public function test_url()
+    {
+        $url = Validate::url("hello");
+        $this->assertFalse($url);
+
+        $url = Validate::url("https://google.com");
+        $this->assertTrue($url);
+    }
+
+    public function test_password()
+    {
+        $password = Validate::password('1234567');
+        $this->assertTrue($password);
+
+        $password = Validate::password('1234567a');
+        $this->assertFalse($password);
     }
 }
